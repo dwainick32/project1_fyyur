1fbd27ef7126fbf36844b77a8fbfe37f5b75778f
David <dmw2442@outlook.com>
2019-09-24 13:01:39 -0400

    new commit

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/Project1_fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/Project1_fyyur'
